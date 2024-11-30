c54697db43ca2f17f50e2ad7f04a3c05347cce70
Sanduni Nisansala <139616844+Sanduni0601@users.noreply.github.com>
2024-11-30 10:47:09 +0000

    Set paths to each component in app.js

--- day08_1/src/App.js
+++ day08_1/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link, NavLink} from "react-router-dom";
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import Contactus from './components/ContactUs';
 
 function App() {
   return (
     <div className="App">
-     
+     <Router>
+      <Routes>
+        <Route path = '/home' element = {<Home/>}/>
+        <Route path = '/about' element = {<AboutUs/>}/>
+        <Route path = '/contact' element = {<Contactus/>}/>
+      </Routes>
+      </Router>
     </div>
   );
 }
