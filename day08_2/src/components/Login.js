9a0f6102fe285d13671b62db389ba286b148b8f5
Sanduni Nisansala <139616844+Sanduni0601@users.noreply.github.com>
2024-11-30 11:29:29 +0000

    set onChange function

--- day08_2/src/components/Login.js
+++ day08_2/src/components/Login.js
@@ -3,7 +3,13 @@ import {useNavigate} from 'react-router-dom';
 
 export default function Login(){
 const navigate = useNavigate();
-
+const [user,setUser] = useState({username:"", password:"",error:""})
+const getTextInput = (e) =>{
+    const {name,value} = e.target;
+    setUser((prevUser)=>({
+        ...prevUser,[name]:value,
+    }))
+}
 
 return(
 <>
@@ -11,11 +17,11 @@ return(
 <table>
     <tr>
         <td>Username: </td>
-        <td><input type = "text" name = "username"/></td>
+        <td><input type = "text" name = "username" onChange={getTextInput}/></td>
     </tr>
     <tr>
         <td>Password: </td>
-        <td><input type = "password" name = "password"/></td>
+        <td><input type = "password" name = "password" onChange={getTextInput}/></td>
     </tr>
 </table>
 <button>Login</button>
